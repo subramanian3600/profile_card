dce11fec5046700ca0f55f3ccc58e8fd6a17c9e2
subramanian3600 <subramanian.s.v.2017.ece@ritchennai.edu.in>
2020-11-19 11:37:06 +0530

    preloader

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',()=>{
+    let preloader=document.querySelector('.loader');
+    let bg=document.querySelector('.loader_background')
+    preloader.classList.add('loader_finish');
+    bg.classList.add('loader_background_none');
+});
